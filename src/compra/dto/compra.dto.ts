a16d8582b0e6fdc044c074eed448cd502d38aa22
Skerme <skerme@gmail.com>
2022-11-24 23:38:25 -0400

    inicio

---
+++ src/compra/dto/compra.dto.ts
@@ -0,0 +1,24 @@
+
+export class CompraDto {
+  constructor(
+
+    public nome: string,
+    public cpf: string,
+    public cnpj: string,
+    public email: string,
+    public bairro: string,
+    public cep: string,
+    public preco:  number,
+    public logradouro: string,
+    public numero: string,
+    public pontoReferencia: string,
+    public telefone: string,
+    public dataInicio: Date,
+    public dataFim: Date,
+    public estado: string,
+    
+    
+  )  {}
+}
+
+
